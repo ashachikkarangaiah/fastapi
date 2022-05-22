9882e61d897fca1342bf63a6a68ed2f35cc6e11a
ashachikkarangaiah <ashacmahesh@gmail.com>
2022-05-22 13:37:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/predict")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+@app.get("/predict")
+async def root():
+    return {"message": "Hello World"}
